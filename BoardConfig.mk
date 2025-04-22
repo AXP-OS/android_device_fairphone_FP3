c1f593d55a107676ea220a302377ed99b11664a1
steadfasterX <steadfasterX@users.noreply.github.com>
2025-04-22 11:05:29 +0200

    mk: enable AXP.OS boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -291,6 +291,6 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 # inherit from the proprietary version
 -include vendor/fairphone/FP3/BoardConfigVendor.mk
 
-# even though we include vendor/axp/config/common.mk we need to include BoardConfig (after the above
-# definitons & includes), too so we we can make use of the conditions within
+# even though we include vendor/axp/config/common.mk we need to include AXP's own BoardConfig
+# (after the above definitions & includes), too so we we can make use of the conditions within
 include vendor/axp/BoardConfigVendor.mk
